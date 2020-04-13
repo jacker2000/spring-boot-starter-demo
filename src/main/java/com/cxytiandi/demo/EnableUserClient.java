6ef5a5cda25b8a5edff2eaa7d5095b8116b83953
gf <fangao1829@126.com>
2020-04-13 17:34:20 +0800

    第一次update

---
+++ src/main/java/com/cxytiandi/demo/EnableUserClient.java
@@ -0,0 +1,14 @@
+package com.cxytiandi.demo;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({UserAutoConfigure.class})
+public @interface EnableUserClient {
+
+}
